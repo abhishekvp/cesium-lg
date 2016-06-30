7ea72a606880d4d6492ae5a3c5e71b91bfcaac91
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2016-06-30 15:59:14 +0530

    Replaced JSON messages with Protobuffers

--- server.js
+++ server.js
@@ -4,48 +4,95 @@
     var CONFIG = require('./js/node-config.js');
     var http = require('http');
     var dgram = require('dgram');
+    var ws = require("ws");
     var WebSocketServer = require("websocket").server;
+    var ProtoBuf = require("protobufjs");
+    var path = require("path");
+
+    var builder = ProtoBuf.loadProtoFile(path.join(__dirname, "cesium.proto")),
+        Message = builder.build("Message");
 
 
-    function handleRequest(request, response) {
-        response.end('It Works!! Path Hit: ' + request.url);
-    }
-
-    var server = http.createServer(handleRequest);
+    var server = http.createServer(function(req, res) {
+        var file = null,
+            type = "text/html";
+        if (req.url == "/") {
+            file = "index.html";
+        } else if (/^\/(\w+(?:\.min)?\.(?:js|html|proto))$/.test(req.url)) {
+            file = req.url.substring(1);
+            if (/\.js$/.test(file)) {
+                type = "text/javascript";
+            }
+        }
+        if (file) {
+            fs.readFile(path.join(__dirname, file), function(err, data) {
+                if (err) {
+                    res.writeHead(500, {
+                        "Content-Type": type
+                    });
+                    res.end("Internal Server Error: " + err);
+                } else {
+                    res.writeHead(200, {
+                        "Content-Type": type
+                    });
+                    res.write(data);
+                    res.end();
+                    console.log("Served " + file);
+                }
+            });
+        } else {
+            res.writeHead(404, {
+                "Content-Type": "text/html"
+            });
+            res.end("Not Found");
+        }
+    });
 
     server.listen(CONFIG.NODE_SERVER_PORT, function() {
         console.log("WS Relay Server listening on " + CONFIG.NODE_SERVER_IP + ":" + CONFIG.NODE_SERVER_PORT);
     });
 
 
-    var wsServer = new WebSocketServer({
-        'httpServer': server
+    var wsServer = new ws.Server({
+        server: server
     });
 
 
     var count = 0;
     var wsClients = {};
 
-    wsServer.on("request", function(request) { // Port 8081
-        var connection = request.accept('echo-protocol', request.origin);
+    wsServer.on("connection", function(socket) { // Port 8081
+
         //Keep count of all connected clients	
         var id = count++;
         //Store connection object for each of the clients
-        wsClients[id] = connection;
+        wsClients[id] = socket;
         console.log((new Date()) + ' Connection accepted [' + id + ']');
 
+
         //On receiving message (Camera Properties) from the Master
-        connection.on("message", function(message) {
-            console.log(message.utf8Data);
-            //Broadcast camera properties to connected clients
-            for (var i in wsClients) {
-                if (wsClients[i] != connection)
-                    wsClients[i].send(message.utf8Data);
+        socket.on("message", function(data, flags) {
+            if (flags.binary) {
+                try {
+                    // Decode the Message
+                    var msg = Message.decode(data);
+                    console.log("Received: " + msg.msgtype);
+                    //Broadcast camera properties to connected clients
+                    for (var i in wsClients) {
+                        if (wsClients[i] != socket)
+                            wsClients[i].send(msg.toBuffer());
+                    }
+
+                } catch (err) {
+                    console.log("Processing failed:", err);
+                }
+            } else {
+                console.log("Not binary data");
             }
-        })
-        connection.on("close", function(reasonCode, description) {
+        });
+        socket.on("close", function(reasonCode, description) {
             delete wsClients[id];
-            console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+            console.log((new Date()) + ' Peer ' + socket.remoteAddress + ' disconnected.');
         })
     });
 
@@ -90,16 +137,23 @@
         var msgArray = String(message).split(',');
 
         //if(clients[i]!=connection)
-        var lat = msgArray[1];
-        var lon = msgArray[2];
-        var alt = msgArray[3];
-        var heading = msgArray[4] * Math.PI / 180;
-        var pitch = (msgArray[5] - 90) * Math.PI / 180;
-        var roll = msgArray[6] * Math.PI / 180;
-        var msgToWSClients = '{"msg-type":"ge-cam", "lon":' + lon + ',"lat":' + lat + ',"ht":' + alt + ',"heading":' + heading + ',"pitch":' + pitch + ',"roll":' + roll + '}';
+        var lat = parseFloat(msgArray[1]);
+        var lon = parseFloat(msgArray[2]);
+        var alt = parseFloat(msgArray[3]);
+        var heading = parseFloat(msgArray[4]) * Math.PI / 180;
+        var pitch = (parseFloat(msgArray[5]) - 90) * Math.PI / 180;
+        var roll = parseFloat(msgArray[6]) * Math.PI / 180;
+	var msgToWSClients = new Message();
+	msgToWSClients.msgtype = "ge-cam";
+	msgToWSClients.lon = lon;
+	msgToWSClients.lat = lat;
+	msgToWSClients.ht = alt;
+	msgToWSClients.heading = heading;
+	msgToWSClients.pitch = pitch;
+	msgToWSClients.roll = roll;
 
         for (var i in wsClients) {
-            wsClients[i].send(msgToWSClients);
+            wsClients[i].send(msgToWSClients.toBuffer());
         }
 
     });

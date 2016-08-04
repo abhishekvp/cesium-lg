77959adc4c097eb54d51fdda9f15d83900f9e7cd
Abhishek Potnis <abhishekp.bugzilla@gmail.com>
2016-08-04 13:35:40 +0530

    Removed unneccessary code for handling req and resp from/to server

--- server.js
+++ server.js
@@ -12,42 +12,7 @@
 
     var CesiumSync = ProtoBuf.loadProtoFile("cesiumsync.proto").build("CesiumSync");
 
-
-
-    var server = http.createServer(function(req, res) {
-        var file = null,
-            type = "text/html";
-        if (req.url == "/") {
-            file = "index.html";
-        } else if (/^\/(\w+(?:\.min)?\.(?:js|html|proto))$/.test(req.url)) {
-            file = req.url.substring(1);
-            if (/\.js$/.test(file)) {
-                type = "text/javascript";
-            }
-        }
-        if (file) {
-            fs.readFile(path.join(__dirname, file), function(err, data) {
-                if (err) {
-                    res.writeHead(500, {
-                        "Content-Type": type
-                    });
-                    res.end("Internal Server Error: " + err);
-                } else {
-                    res.writeHead(200, {
-                        "Content-Type": type
-                    });
-                    res.write(data);
-                    res.end();
-                    console.log("Served " + file);
-                }
-            });
-        } else {
-            res.writeHead(404, {
-                "Content-Type": "text/html"
-            });
-            res.end("Not Found");
-        }
-    });
+    var server = http.createServer();
 
     server.listen(CONFIG.NODE_SERVER_PORT, function() {
         console.log("WS Relay Server listening on " + CONFIG.NODE_SERVER_IP + ":" + CONFIG.NODE_SERVER_PORT);
